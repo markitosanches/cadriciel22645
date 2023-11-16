c00b55d5bca44d88650176fb5a11cfc41da2841c
Marcos <markitosanches@gmail.com>
2023-11-16 11:50:59 -0500

    seance 2

--- blog/routes/web.php
+++ blog/routes/web.php
@@ -1,6 +1,8 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\BlogController;
+
 
 /*
 |--------------------------------------------------------------------------
@@ -16,3 +18,7 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+
+Route::get('/home', [BlogController::class, 'index']);
+Route::get('/about', [BlogController::class, 'about']);
